b54a60c903d3dbb5369e708784cd64802ba02a34
msrezaie <79583632+msrezaie@users.noreply.github.com>
2026-02-19 16:03:00 -0500

    initial build (tammam's progress)

---
+++ city_scrapers/spiders/colum_boe.py
@@ -0,0 +1,126 @@
+from urllib import response
+from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+import random
+import scrapy
+
+
+class ColumBoeSpider(CityScrapersSpider):
+    name = "colum_boe"
+    agency = "Columbus Board of Education"
+    timezone = "America/Chicago"
+    api_url = "https://go.boarddocs.com/oh/columbus/Board.nsf/BD-GetMeetingsList?open&0.{random_digit}" # noqa
+    detail_url = "https://go.boarddocs.com/oh/columbus/Board.nsf/BD-GetMeeting?open&0.{random_digit}" # noqa
+    boarddocs_committee_id = "A9HCVU32F33A"
+
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url=self.api_url.format(random_digit=random.randint(1000000000000000, 9999999999999999)),
+            method="POST",
+            body=f"current_committee_id={self.boarddocs_committee_id}",
+            callback=self._parse_meetings_list,
+        )
+
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+    }
+    
+    def _parse_meetings_list(self, response):
+
+        meetings = response.json()
+
+        # for meeting in meetings:
+        meeting_id = meetings[0].get("unique")
+        yield scrapy.Request(
+            url=self.detail_url,
+            method="POST",
+            body=f"current_committee_id={self.boarddocs_committee_id}&id={meeting_id}",
+            callback=self.parse,
+        )
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        print("RESPONSE: ", response.text)
+
+        # for item in response.css(".meetings"):
+        #     meeting = Meeting(
+        #         title=self._parse_title(item),
+        #         description=self._parse_description(item),
+        #         classification=self._parse_classification(item),
+        #         start=self._parse_start(item),
+        #         end=self._parse_end(item),
+        #         all_day=self._parse_all_day(item),
+        #         time_notes=self._parse_time_notes(item),
+        #         location=self._parse_location(item),
+        #         links=self._parse_links(item),
+        #         source=self._parse_source(response),
+        #     )
+
+        #     meeting["status"] = self._get_status(meeting)
+        #     meeting["id"] = self._get_id(meeting)
+
+        yield self._parse_title(response)
+        yield self._parse_description(response)
+        yield self._parse_classification(response)
+
+
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        title = item.css(".meeting-name::text").get()
+        print("TITLE >>>> : ", title)
+        return title
+
+    def _parse_description(self, item):
+        """Parse or generate meeting description."""
+        description= item.css(".meeting-description::text").getall()
+        print("DESCRIPTION >>>> : ", description)
+        return ""
+
+    def _parse_classification(self, item):
+        """Parse or generate classification from allowed options."""
+        print("TITLE in classification>>>> : ", self._parse_title(item))
+        if "BOARD" in self._parse_title(item):
+            print("CLASSIFICATION >>>> : ", "Board")
+            return "Board"
+        else:
+            print("CLASSIFICATION >>>> : ", NOT_CLASSIFIED)
+            return NOT_CLASSIFIED
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        return None
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        return None
+
+    def _parse_time_notes(self, item):
+        """Parse any additional notes on the timing of the meeting"""
+        return ""
+
+    def _parse_all_day(self, item):
+        """Parse or generate all-day status. Defaults to False."""
+        return False
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        return {
+            "address": "",
+            "name": "",
+        }
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        return [{"href": "", "title": ""}]
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
