a0207f8de3b4c08911168cf2cd531a4870dd6c3a
msrezaie <79583632+msrezaie@users.noreply.github.com>
2026-02-24 14:36:37 -0500

    organized the code

--- city_scrapers/spiders/colum_boe.py
+++ city_scrapers/spiders/colum_boe.py
@@ -1,124 +1,93 @@
-import random
-from urllib import response
-
-import scrapy
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from urllib.parse import urlencode
+from city_scrapers_core.constants import BOARD, PASSED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
-from dateutil.parser import parse
+import random
+import scrapy
+from dateutil.parser import parse as dateparse
 
 
 class ColumBoeSpider(CityScrapersSpider):
+    # just for easy access
+    # https://go.boarddocs.com/oh/columbus/Board.nsf/Public
+
     name = "colum_boe"
     agency = "Columbus Board of Education"
     timezone = "America/Chicago"
     api_url = "https://go.boarddocs.com/oh/columbus/Board.nsf/BD-GetMeetingsList?open&0.{random_digit}"  # noqa
     detail_url = "https://go.boarddocs.com/oh/columbus/Board.nsf/BD-GetMeeting?open&0.{random_digit}"  # noqa
+
+    get_agenda_url = "https://go.boarddocs.com/oh/columbus/Board.nsf/BD-GetAgenda?open&0.{random_digit}"
+    agenda_url = "https://go.boarddocs.com/oh/columbus/Board.nsf/BD-GetAgendaItem?open&0.{random_digit}"
+
     boarddocs_committee_id = "A9HCVU32F33A"
 
-    def start_requests(self):
-        yield scrapy.Request(
-            url=self.api_url.format(
-                random_digit=random.randint(1000000000000000, 9999999999999999)
-            ),
-            method="POST",
-            body=f"current_committee_id={self.boarddocs_committee_id}",
-            callback=self._parse_meetings_list,
-        )
+    random_digit = random.randint(10**14, 10**15 - 1)
 
     custom_settings = {
         "ROBOTSTXT_OBEY": False,
     }
 
-    def _parse_meetings_list(self, response):
+    def start_requests(self):
+        yield scrapy.Request(
+            url=self.api_url.format(random_digit=self.random_digit),
+            method="POST",
+            body=f"current_committee_id={self.boarddocs_committee_id}",
+            callback=self._get_meeting_detail,
+        )
 
+    def _get_meeting_detail(self, response):
         meetings = response.json()
 
-        # for meeting in meetings:
-        meeting_id = meetings[0].get("unique")
+        for meeting in meetings:
+            meeting_id = meeting.get("unique")
+            yield scrapy.Request(
+                url=self.detail_url.format(random_digit=self.random_digit),
+                method="POST",
+                body=f"current_committee_id={self.boarddocs_committee_id}&id={meeting_id}",
+                meta={"meeting_id": meeting_id},
+                callback=self._get_agenda,
+            )
+            break
+
+    def _get_agenda(self, response):
         yield scrapy.Request(
-            url=self.detail_url,
+            url=self.agenda_url.format(random_digit=self.random_digit),
             method="POST",
-            body=f"current_committee_id={self.boarddocs_committee_id}&id={meeting_id}",
+            body=f"current_committee_id={self.boarddocs_committee_id}&id=DQ3VPZ80F262",
+            meta={"detail_response": response},
             callback=self.parse,
         )
 
     def parse(self, response):
-        """
-        `parse` should always `yield` Meeting items.
+        meeting = Meeting(
+            title=self._parse_title(response),
+            description=self._parse_description(response),
+            classification=BOARD,
+            start=self._parse_start(response),
+            end=None,
+            all_day=False,
+            time_notes="",
+            location=self._parse_location(response),
+            links=self._parse_links(response),
+            source=response.url,
+        )
 
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
-        needs.
-        """
-        print("RESPONSE: ", response.text)
-
-        # for item in response.css(".meetings"):
-        #     meeting = Meeting(
-        #         title=self._parse_title(item),
-        #         description=self._parse_description(item),
-        #         classification=self._parse_classification(item),
-        #         start=self._parse_start(item),
-        #         end=self._parse_end(item),
-        #         all_day=self._parse_all_day(item),
-        #         time_notes=self._parse_time_notes(item),
-        #         location=self._parse_location(item),
-        #         links=self._parse_links(item),
-        #         source=self._parse_source(response),
-        #     )
-
-        #     meeting["status"] = self._get_status(meeting)
-        #     meeting["id"] = self._get_id(meeting)
-
-        # yield self._parse_title(response)
-        # yield self._parse_description(response)
-        # yield self._parse_classification(response)
-        yield self._parse_start(response)
+        # meeting["status"] = self._get_status(meeting)
+        meeting["status"] = PASSED
+        meeting["id"] = self._get_id(meeting)
 
     def _parse_title(self, item):
-        """Parse or generate meeting title."""
         title = item.css(".meeting-name::text").get()
-        print("TITLE >>>> : ", title)
         return title
 
     def _parse_description(self, item):
-        """Parse or generate meeting description."""
-        description = item.css(".meeting-description::text").getall()
-        print("DESCRIPTION >>>> : ", description)
-        return ""
-
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        print("TITLE in classification>>>> : ", self._parse_title(item))
-        if "BOARD" in self._parse_title(item):
-            print("CLASSIFICATION >>>> : ", "Board")
-            return "Board"
-        else:
-            print("CLASSIFICATION >>>> : ", NOT_CLASSIFIED)
-            return NOT_CLASSIFIED
+        desc_text = item.css(".meeting-description::text").getall()
+        return " ".join(desc_text).strip()
 
     def _parse_start(self, item):
-        """Parse start datetime as a naive datetime object."""
-        date = item.css(".meeting-date::text").get()
-        print("DATE >>>> : ", date)
-        parsed_date = parse(date)
-        print("PARSED DATE >>>> : ", parsed_date)
-        description = item.css(".meeting-description::text").getall()
-        # print("DESCRIPTION >>>> : ", description)
-        # parsed_time = parse(time)
-        print("PARSED TIME >>>> : ", description)
-        # return parsed_date
-
-    def _parse_end(self, item):
-        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
-
-    def _parse_time_notes(self, item):
-        """Parse any additional notes on the timing of the meeting"""
-        return ""
-
-    def _parse_all_day(self, item):
-        """Parse or generate all-day status. Defaults to False."""
-        return False
+        start = item.css(".meeting-date::text").get()
+        return dateparse(start)
 
     def _parse_location(self, item):
         """Parse or generate location."""
@@ -128,9 +97,4 @@ class ColumBoeSpider(CityScrapersSpider):
         }
 
     def _parse_links(self, item):
-        """Parse or generate links."""
-        return [{"href": "", "title": ""}]
-
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
+        return []
